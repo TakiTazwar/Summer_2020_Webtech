9d7751fe4e61bdb72991894c8a17ac818c7f4f9e
TakiTazwar <57331465+TakiTazwar@users.noreply.github.com>
2020-08-09 08:43:06 +0600

    Name Validation

--- MID_PROJECT/regSave.php
+++ MID_PROJECT/regSave.php
@@ -1,7 +1,58 @@
 <?php
-
-	if (isset($_POST['submit'])) 
+if(isset($_REQUEST['name']))
+{
+	$Name=$_REQUEST['name'];
+	if(strlen($Name)>0)
 	{
+		if(str_word_count($Name)>=2)
+		{
+			if(($Name[0]>='A' && $Name[0]<='Z') || ($Name[0]>='a' && $Name[0]<='z'))
+			{
+				//echo "pokpok";
+				$i=0;
+				while($i<strlen($Name))
+				{
+					if($Name[$i]<'A' && $Name[$i]!=' ' && $Name[$i]!='.' && $Name[$i]!='-')
+					{
+						$Name="Invalid Name";
+						break;
+					}
+					elseif ($Name[$i]>'Z') 
+					{
+						if($Name[$i]<'a')
+						{
+							$Name="Invalid Name";
+							break;
+						}
+						elseif ($Name[$i]>'z') {
+							$Name="Invalid Name";
+							break;
+						}
+					}
+					$i=$i+1;
+					//echo "pokpok<br>";
+				}
+			
+			}
+			else
+			{
+				$Name="Invalid Name";
+			}
+		}
+		else
+		{
+			$Name="Invalid Name";
+		}
+		//echo str_word_count($Name);
+	}
+	else
+	{
+		$Name="Invalid Name";
+	}
+	if($Name!="Invalid Name")
+	{
+		if (isset($_POST['submit'])) 
+		{
 		# code...
 		$conn = mysqli_connect('127.0.0.1', 'root', '', 'foodDeliverManagementSystem');
 		$sql= 'select * from registration where username="'.$_POST['username'].'"';
@@ -22,17 +73,28 @@
 				echo "Password doesn't match";
 			}
 			mysqli_close($conn);
+			}
+			else
+			{
+				echo "Username already exists";
+			}
 		}
 		else
 		{
-			echo "Username already exists";
+			header("location: registration.html");
 		}
 	}
 	else
 	{
-		header("location: registration.html")
+		echo "does not work";
 	}
-	
+}
+else
+{
+	echo "No name";
+	echo $_POST['name'];
+}
+
 
 ?>
 
