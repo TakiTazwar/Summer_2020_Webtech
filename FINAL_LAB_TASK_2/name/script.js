0d72747fc8a0e6b159c54b4ae84ffb000b176ab8
TakiTazwar <57331465+TakiTazwar@users.noreply.github.com>
2020-08-24 21:37:26 +0600

    word count,specific letter

--- FINAL_LAB_TASK_2/name/script.js
+++ FINAL_LAB_TASK_2/name/script.js
@@ -1,25 +1,47 @@
 function val()
 {
-	
-	var a =document.getElementById('name').value;
-	if(a=="")
+	var userName=document.getElementById('name').value;
+	if(userName=="")
 	{
 		document.getElementById('msg').innerHTML="Field cannot be empty";
 		return false;
 	}
 	else
 	{
-		if(!((a[0]>='A' && a[0]<='Z') ||(a[0]>='a' && a[0]<='z')))
+		if((userName[0]>='A' && userName[0]<='Z') ||(userName[0]>='a' && userName[0]<='z'))
+		{
+			if(userName.split(" ").length>=2)
+			{
+				var counter=0;
+				while(counter<userName.length)
+				{
+					if(!((userName[counter]>='A' && userName[counter]<='Z') ||(userName[counter]>='a' && userName[counter]<='z') || userName[counter]=='.' || userName[counter]=='-' || userName[counter]==' '))
+					{
+						document.getElementById('msg').innerHTML="Name can only contain A-Z, a-z, . and -";
+						return false;
+					}
+					counter=counter+1;
+				}
+				return true;
+			}
+			else
+			{
+				document.getElementById('msg').innerHTML="Name must contain 2 words";
+				return false;
+			}
+		}
+		else
 		{
 			document.getElementById('msg').innerHTML="Name must start with a letter";
 			return false;
 		}
 	}
+	return false;
 }
 function remover()
 {
-	var a =document.getElementById('name').value;
-	if(a!="")
+	var userName =document.getElementById('name').value;
+	if(userName!="")
 	{
 		document.getElementById('msg').innerHTML="";
 	}
