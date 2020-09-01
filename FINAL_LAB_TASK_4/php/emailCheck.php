326ddd633f66e47f0f9a9087ab17ef3a988092d5
TakiTazwar <57331465+TakiTazwar@users.noreply.github.com>
2020-09-01 08:56:37 +0600

    reactive

---
+++ FINAL_LAB_TASK_4/php/emailCheck.php
@@ -0,0 +1,15 @@
+<?php 
+	session_start();
+	//require_once('../php/session_header.php');
+	require_once('../service/userService.php');
+
+
+	//add user
+	if(isset($_POST['email'])){
+
+		echo "Yes";
+		
+	}
+
+
+?>
